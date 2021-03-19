ff7cae78c0bf4b5775cf1ad36fb99a704eb7d254
Rose Peck <olaf223224@gmail.com>
2021-03-19 18:10:34 -0500

    Add jumping

--- src/player.rs
+++ src/player.rs
@@ -1,17 +1,23 @@
-use bevy::prelude::*;
-use crate::physics::{Collides, Gravity, PhysicsBundle, Solid};
+use bevy::{math::vec2, prelude::*};
+use crate::physics::{Collides, Gravity, PhysicsBundle, Solid, Velocity};
 use crate::AssetLoadingState;
-
-pub struct Player;
 
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
 	fn build(&self, app: &mut AppBuilder) {
 		app
-			.add_system_set(SystemSet::on_enter(AssetLoadingState::Finished).with_system(setup_player.system()));
+			.add_system_set(SystemSet::on_enter(AssetLoadingState::Finished)
+				.with_system(setup_player.system())
+			)
+			.add_system_set(SystemSet::on_update(AssetLoadingState::Finished)
+				.with_system(control_player.system())
+			);
 	}
 }
+
+pub struct Player;
+
 fn setup_player(
 	mut commands: Commands,
     asset_server: Res<AssetServer>,
@@ -24,6 +30,17 @@ fn setup_player(
             material: materials.add(texture_handle.into()),
             ..Default::default()
         })
-		.with_bundle((Collides, Solid, Gravity))
+		.with_bundle((Collides, Solid, Gravity, Player))
 		.with_bundle(PhysicsBundle::player());
+}
+
+fn control_player(
+	mut query: Query<&mut Velocity, With<Player>>,
+	keyboard_input: Res<Input<KeyCode>>,
+) {
+	if keyboard_input.just_pressed(KeyCode::Space) {
+		let mut velocity = query.single_mut().unwrap();
+		
+		velocity.0 = vec2(0.0, 500.0);
+	}
 }
